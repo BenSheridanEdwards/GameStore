bfd377e09d0b0ad9e2dbdffba798adf565ca2e8c
bensheridanedwards <bensheridanedwards@gmail.com>
2022-10-13 14:33:07 +0300

    api: export miragejs server as function to enable environment setting for tests.

--- src/mock/server.js
+++ src/mock/server.js
@@ -1,16 +1,20 @@
 import { Server } from "miragejs";
 import { games } from "./games";
 
-let server = new Server();
+export function makeServer({ environment = "development" } = {}) {
+  let server = new Server({ environment });
 
-server.get("/api/games", { games });
+  server.get("/api/games", { games });
 
-games.forEach((game) => {
-  server.get(`/api/games/${game.id}`, game);
-});
+  games.forEach((game) => {
+    server.get(`/api/games/${game.id}`, game);
+  });
 
-server.get("/api/rates", {
-  USD: 1,
-  EUR: 0.8738967054,
-  GBP: 0.7869876781,
-});
+  server.get("/api/rates", {
+    USD: 1,
+    EUR: 0.8738967054,
+    GBP: 0.7869876781,
+  });
+
+  return server;
+}
