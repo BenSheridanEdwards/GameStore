1f4c9e8901c3823161331652f0cf5e5ad1b1f281
bensheridanedwards <bensheridanedwards@gmail.com>
2022-10-13 14:59:46 +0300

    refactor/style: game details card - Quantity component.

--- src/components/GameDetailsCard/components/Quantity/Quantity.tsx
+++ src/components/GameDetailsCard/components/Quantity/Quantity.tsx
@@ -1,33 +1,40 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import Button from "components/Button/Button";
 import { ReactComponent as AddIcon } from "assets/icons/add.svg";
 import { ReactComponent as SubtractIcon } from "assets/icons/subtract.svg";
+import "./styles.css";
 
-type QuantityProps = {
+interface QuantityProps {
   value: number;
   onChange: (newValue: number) => void;
-};
+}
 
 const Quantity = ({ value, onChange }: QuantityProps) => {
+  const [quantity, setQuantity] = useState(value);
+
   const handleDecreaseClick = useCallback(() => {
-    onChange(value - 1);
-  }, [onChange, value]);
+    quantity > 1 && setQuantity(quantity - 1);
+  }, [quantity]);
 
   const handleIncreaseClick = useCallback(() => {
-    onChange(value + 1);
-  }, [onChange, value]);
+    setQuantity(quantity + 1);
+  }, [quantity]);
 
   return (
     <div className="Quantity">
-      <div className="Quantity__label">Quantity</div>
-      <div>
-        <Button onClick={handleDecreaseClick} variant="secondary">
-          <SubtractIcon />
-        </Button>
-        <div>{value}</div>
-        <Button onClick={handleIncreaseClick} variant="secondary">
-          <AddIcon />
-        </Button>
+      <label>Quantity</label>
+      <div className="Quantity__Controls">
+        <Button
+          icon={<SubtractIcon />}
+          onClick={handleDecreaseClick}
+          variant="icon"
+        />
+        <div className="Quantity__Value">{quantity}</div>
+        <Button
+          icon={<AddIcon />}
+          onClick={handleIncreaseClick}
+          variant="icon"
+        />
       </div>
     </div>
   );
