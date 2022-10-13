8f5b6d78fcdfb078da2b9d592f2006c3723d1d17
bensheridanedwards <bensheridanedwards@gmail.com>
2022-10-13 15:17:10 +0300

    refactor/ game details card -  AddToBasketButton component.

--- src/components/GameDetailsCard/components/AddToBasketButton/AddToBasketButton.tsx
+++ src/components/GameDetailsCard/components/AddToBasketButton/AddToBasketButton.tsx
@@ -3,14 +3,14 @@ import { ReactComponent as PlusIcon } from "assets/icons/plus.svg";
 import { ReactComponent as CheckIcon } from "assets/icons/check.svg";
 import Button from "components/Button/Button";
 
-type AddToBasketButtonProps = {
-  isAdded: boolean;
+interface AddToBasketButtonProps {
+  isAdded?: boolean;
   onClick: () => void;
-};
+}
 
 const AddToBasketButton = ({ isAdded, onClick }: AddToBasketButtonProps) => {
-  const icon = isAdded ? <PlusIcon /> : <CheckIcon />;
-  const label = isAdded ? "Add to basket" : "Added";
+  const icon = isAdded ? <CheckIcon /> : <PlusIcon />;
+  const label = isAdded ? "Added" : "Add to basket";
   const variant = isAdded ? undefined : "primary";
 
   return (
