4b090179a8b88f46ca035bb920440a7d9ed08a62
bensheridanedwards <bensheridanedwards@gmail.com>
2022-10-14 12:43:13 +0300

    feat: create checkout page.

--- src/pages/CheckoutPage/CheckoutPage.tsx
+++ src/pages/CheckoutPage/CheckoutPage.tsx
@@ -1,10 +1,21 @@
-import React, { memo } from "react";
+import React, { memo, useContext } from "react";
 import Layout from "../../components/Layout/Layout";
 import Button from "../../components/Button/Button";
 import { useHistory } from "react-router-dom";
 import "./styles.css";
+import StoreContext from "contexts/StoreContext";
+import GameListCardWrapper from "components/GameDetailsCard/components/GameDetailsCardWrapper/GameDetailsCardWrapper";
+import ReleaseDateAndTitle from "components/GameDetailsCard/components/ReleaseDateAndTitle/ReleaseDateAndTitle";
+import Price from "components/GameDetailsCard/components/Price/Price";
+import Quantity from "components/GameDetailsCard/components/Quantity/Quantity";
+import RemoveFromCartButton from "components/GameDetailsCard/components/RemoteFromCartButton/RemoveFromCartButton";
+import OrderDetails from "components/OrderDetails/OrderDetails";
+import CurrencyContext from "contexts/CurrencyContext";
 
 const CheckoutPage = memo(() => {
+  const { storeGames } = useContext(StoreContext);
+  const { exchangeRates, selectedCurrency } = useContext(CurrencyContext);
+
   const history = useHistory();
 
   return (
@@ -16,12 +27,53 @@ const CheckoutPage = memo(() => {
       }}
     >
       <div className="CheckoutPage-container">
-        <div className="CheckoutPage-GameList-container">[CheckoutPage]</div>
+        <div className="CheckoutPage-GameList-container">
+          {storeGames &&
+            storeGames.length > 0 &&
+            storeGames
+              .filter((game) => game.inBasket)
+              .map(
+                ({
+                  artworkUrl,
+                  releaseDate,
+                  price,
+                  title,
+                  id,
+                  inBasket,
+                  quantity,
+                }) => {
+                  return inBasket ? (
+                    <GameListCardWrapper
+                      artworkUrl={artworkUrl}
+                      title={title}
+                      id={id}
+                      key={id}
+                    >
+                      <ReleaseDateAndTitle
+                        releaseDate={releaseDate}
+                        title={title}
+                      />
+                      <Quantity gameId={id} quantity={quantity} />
+                      <Price
+                        value={price}
+                        exchangeRates={exchangeRates}
+                        selectedCurrency={selectedCurrency}
+                      />
+                      <RemoveFromCartButton gameId={id} />
+                    </GameListCardWrapper>
+                  ) : null;
+                }
+              )}
+        </div>
 
         <div className="CheckoutPage-Overview-container">
-          [Order Details]
+          <OrderDetails />
           <hr className="CheckoutPage-Divider" />
-          <Button variant="link" fullWidth onClick={() => history.push("/")}>
+          <Button
+            variant="link"
+            fullWidth
+            onClick={() => history.push("/list")}
+          >
             Back to overview
           </Button>
         </div>
