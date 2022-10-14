026f1b8f31cad1ecca35ec3c6df3d2569e005ae7
bensheridanedwards <bensheridanedwards@gmail.com>
2022-10-14 12:44:30 +0300

    feat: create remove from cart button.

---
+++ src/components/GameDetailsCard/components/RemoteFromCartButton/RemoveFromCartButton.tsx
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { ReactComponent as TrashIcon } from "assets/icons/trash.svg";
+import Button from "components/Button/Button";
+import StoreContext from "contexts/StoreContext";
+
+interface RemoveFromBasketButtonProps {
+  gameId: string;
+}
+
+const RemoveFromCartButton = ({ gameId }: RemoveFromBasketButtonProps) => {
+  const { setGames } = useContext(StoreContext);
+
+  const handleClick = () => {
+    setGames((games) => {
+      return games.map((game) => {
+        if (game.id === gameId) {
+          game.inBasket = false;
+          game.quantity = 1;
+        }
+        return game;
+      });
+    });
+  };
+
+  return (
+    <Button icon={<TrashIcon />} variant="secondary" onClick={handleClick}>
+      Remove
+    </Button>
+  );
+};
+
+export default RemoveFromCartButton;
