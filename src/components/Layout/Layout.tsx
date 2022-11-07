7e6a6bd761696b018ed543304def8c0b48e64b11
bensheridanedwards <bensheridanedwards@gmail.com>
2022-11-07 16:34:27 +0000

    refactor: Layout component.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,22 +1,24 @@
 import React, { ReactElement, ReactNode } from "react";
-import { AppBar, AppBarProps } from "../AppBar/AppBar";
-import "./styles.css";
+import { NavBar, NavBarProps } from "../NavBar/NavBar";
+import "./Layout.css";
 
 interface LayoutProps {
-  backButton?: AppBarProps["backButton"];
+  backLink?: NavBarProps["backLink"];
   children: ReactNode;
   title: string;
 }
 
 export function Layout({
-  backButton,
+  backLink,
   children,
   title,
 }: LayoutProps): ReactElement {
   return (
     <div className="Layout">
-      <AppBar headerText={title} backButton={backButton} />
-      <div className="Layout__Content">{children}</div>
+      <header>
+        <NavBar headerText={title} backLink={backLink} />
+      </header>
+      <main className="Layout__mainContent">{children}</main>
     </div>
   );
 }
